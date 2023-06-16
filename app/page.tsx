fc4708b5b7f2feedbb72e5e094e5d28daf7ec347
Alexander Oliver Beck Sommer <alexanderobsommer@gmail.com>
2023-06-16 11:18:36 +0200

    Installed Tailwind Viewer, customized action buttons, etc.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="font-body">
+    <main className="font-body text-14">
       <h2>Test</h2>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,16 @@ module.exports = {
       title: ["Yantramanav"],
       body: ["Roboto"],
     },
+    fontSize: {
+      14: "0.875rem",
+      32: "2rem",
+    },
+    configViewer: {
+      fonts: [
+        "https://fonts.googleapis.com/css2?family=Yantramanav:wght@100;300;400;500;700;900&display=swap",
+        "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap", // <- only 500 will be used
+      ],
+    },
   },
   plugins: [],
 };
