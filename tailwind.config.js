e43935a0a5dfb6bef3b43575cfd9c61a94148dc9
Alexander Oliver Beck Sommer <alexanderobsommer@gmail.com>
2023-06-16 09:56:43 +0200

    Added colors and font-family files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      sm: "375px",
+      // Mobile
+      md: "744px",
+      // Tablet
+      lg: "1440px",
+      // Desktop
+    },
+    colors: {
+      white: "#FFFFFF",
+      white_alt: "#F7F7F7",
+      black: "#202020",
+      red: "#F94144",
+      purple: "#7C41F9",
+      green: "#3ED04D",
+      blue: "#4175F9",
+      yellow: "#F9C74F",
     },
   },
   plugins: [],
-}
+};
