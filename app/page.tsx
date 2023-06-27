395c4269a6a032e59f004598caf172cecc4a8b12
Alexander Oliver Beck Sommer <alexanderobsommer@gmail.com>
2023-06-27 09:39:23 +0200

    Added things?

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,43 @@
+import HomeButton from "@/components/Button/ButtonComponent";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="font-body text-14">
+    <main>
+      <HomeButton
+        text="Read more on About"
+        page="/about"
+        aria="Test"
+        tip="Test"
+      />
+      <h1>Test</h1>
       <h2>Test</h2>
+      <h3>Test</h3>
+      <h4>Test</h4>
+      <h5>Test</h5>
+      <h6>Test</h6>
+      <p>
+        Aute consectetur commodo mollit proident ex aute anim consequat ipsum
+        commodo dolore esse amet dolore. Aliquip aute et commodo ullamco duis
+        irure ullamco aliquip incididunt sit nulla Lorem aliqua adipisicing.
+        Pariatur reprehenderit nostrud anim id minim aliquip eiusmod sit nisi.
+        Voluptate ad labore Lorem ex nulla sunt non nostrud sint irure ea ut.
+        Dolor duis commodo ad tempor ut. Amet cillum incididunt ex ipsum. Sint
+        id ad do enim anim consectetur ea est incididunt adipisicing mollit
+        laboris. Amet consequat voluptate nostrud enim ipsum velit tempor
+        deserunt occaecat id aliquip pariatur reprehenderit. Velit officia ut
+        proident reprehenderit occaecat occaecat pariatur voluptate commodo
+        reprehenderit aliqua eu tempor. Duis do ipsum nulla fugiat nostrud esse
+        laborum elit. Est consectetur aliquip quis Lorem irure irure fugiat anim
+        ad ipsum laboris velit. Pariatur et eiusmod in exercitation non cillum
+        ex esse sint laborum veniam consequat. Ut velit irure consequat irure ex
+        in deserunt nulla tempor fugiat ullamco anim. Voluptate adipisicing
+        dolor ad commodo occaecat exercitation duis. Irure id eiusmod consequat
+        aute voluptate enim est dolor nisi laborum ipsum. Labore pariatur duis
+        nisi sit ipsum ad aliquip nostrud. In fugiat sunt ad ea pariatur elit
+        culpa. Dolor quis id fugiat occaecat ex. Eu adipisicing do irure eu qui
+        aute nisi aliqua eu.
+      </p>
     </main>
   );
 }
--- components/Button/ButtonComponent.tsx
+++ components/Button/ButtonComponent.tsx
@@ -1,7 +1,16 @@
-export default function ButtonComponent() {
+import Link from "next/link";
+
+export default function HomeButton({ text, page, aria, tip }) {
   return (
     <>
-      <h4>ButtonComponent</h4>
+      <Link
+        href={page}
+        aria-label={aria}
+        title={tip}
+        className="bold block w-full border-2 border-solid border-red bg-red px-25 py-8 text-center text-white md:w-fit"
+      >
+        <h4 className="md:text-18">{text}</h4>
+      </Link>
     </>
   );
 }
